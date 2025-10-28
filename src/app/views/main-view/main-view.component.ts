489a0e22c163eb64e88f8c2dbc9a4910f343b4f5
Luis Nafate <luisnafate51@gmail.com>
2025-10-28 13:19:54 -0600

    refactor: reemplazar TrackList por lista de reproduccion fija en vista principal

--- src/app/views/main-view/main-view.component.ts
+++ src/app/views/main-view/main-view.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
-import { TrackListComponent } from '../../components/track-list/track-list.component';
 import { PlayerComponent } from '../../components/player/player.component';
 import { SpotifyService, SpotifyTrack } from '../../services/spotify.service';
 import { MusicStateService } from '../../services/music-state.service';
@@ -14,16 +13,43 @@ import { MusicStateService } from '../../services/music-state.service';
   imports: [
     CommonModule,
     SearchBarComponent,
-    TrackListComponent,
     PlayerComponent
   ],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css'
 })
 export class MainViewComponent implements OnInit, OnDestroy {
-  tracks: SpotifyTrack[] = [];
   selectedTrack: SpotifyTrack | null = null;
   errorMessage: string = '';
+  
+  // Lista de reproduccion fija (simulada)
+  playQueue: any[] = [
+    {
+      name: 'ANTIFRAGILE',
+      artist: 'LE SSERAFIM',
+      image: 'https://i.scdn.co/image/ab67616d00001e023369310bc92d472415369ed6'
+    },
+    {
+      name: 'Impacto',
+      artist: 'Enjambre',
+      image: 'https://i.scdn.co/image/ab67616d00001e0266f128e383614b0aa4df5ebd'
+    },
+    {
+      name: 'UNFORGIVEN',
+      artist: 'LE SSERAFIM',
+      image: 'https://i.scdn.co/image/ab67616d00001e02971bef5fdb2db3fa3e6e3870'
+    },
+    {
+      name: 'Vida En El Espejo',
+      artist: 'Enjambre',
+      image: 'https://i.scdn.co/image/ab67616d00001e0208b5853acded25e1b5ff5115'
+    },
+    {
+      name: 'FEARLESS',
+      artist: 'LE SSERAFIM',
+      image: 'https://i.scdn.co/image/ab67616d00001e02971bef5fdb2db3fa3e6e3870'
+    }
+  ];
   
   private subscriptions = new Subscription();
 
@@ -35,16 +61,10 @@ export class MainViewComponent implements OnInit, OnDestroy {
 
   // Inicializar y suscribirse al estado
   ngOnInit(): void {
-    // Suscribirse a cambios en el estado
+    // Suscribirse solo a la cancion seleccionada
     this.subscriptions.add(
       this.musicState.selectedTrack$.subscribe(track => {
         this.selectedTrack = track;
-      })
-    );
-    
-    this.subscriptions.add(
-      this.musicState.tracks$.subscribe(tracks => {
-        this.tracks = tracks;
       })
     );
   }
@@ -80,6 +100,22 @@ export class MainViewComponent implements OnInit, OnDestroy {
 
   // Cambiar cancion seleccionada y actualizar estado
   onTrackSelected(track: SpotifyTrack): void {
+    this.musicState.setSelectedTrack(track);
+  }
+
+  // Seleccionar cancion de la lista de reproduccion fija
+  selectFromPlayQueue(queueItem: any): void {
+    // Convertir item de playQueue a formato SpotifyTrack
+    const track: any = {
+      id: queueItem.name.toLowerCase().replace(/\s+/g, '-'),
+      name: queueItem.name,
+      artists: [{ name: queueItem.artist }],
+      album: {
+        name: queueItem.name,
+        images: [{ url: queueItem.image }]
+      },
+      preview_url: null
+    };
     this.musicState.setSelectedTrack(track);
   }
 }
